f310a53b7db941e0a774f51d16c7552fb302beb8
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2024-12-29 16:14:52 +0700

    Update db.js
    add something

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
-var hostname = "127.0.0.1";
-var database = "projectPAW";
-var port = "3306";
-var username = "root";
-var password = "";
+var hostname = "gg-d5.h.filess.io";
+var database = "BOBODB_showcareit";
+var port = "3307";
+var username = "BOBODB_showcareit";
+var password = "9551537df099705f889782ac88c02816767cf323";
 
 var con = mysql.createConnection({
   host: hostname,
